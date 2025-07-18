3d9f89628ab6f9a85d0b22edaf747f98b32113c9
MonsterLlama <MonsterLlama@gmail.com>
2025-07-17 17:59:18 -0700

    Added C# ASP.NET Core Web API with minimal controllers project

---
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Kiwi_SDR_Online_Receiver_Logging
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
