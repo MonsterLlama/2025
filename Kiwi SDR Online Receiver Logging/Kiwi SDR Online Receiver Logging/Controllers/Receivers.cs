24300506d0ab8b989fb846a8cb75075e327e04db
MonsterLlama <MonsterLlama@gmail.com>
2025-07-17 18:29:56 -0700

    Add controller support & a simple Receivers controller.

---
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Controllers/Receivers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kiwi_SDR_Online_Receiver_Logging.Controllers
+{
+    [Route("api/[controller]")]
+    public class Receivers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllServers()
+        {
+            // Test Return value
+            return Ok("GetAllServers()");
+        }
+    }
+}
--- Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Program.cs
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Program.cs
@@ -8,10 +8,16 @@ namespace Kiwi_SDR_Online_Receiver_Logging
 
             // Add services to the container.
 
+            // Adds services for controllers to the services collection..
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             app.UseHttpsRedirection();
+
+            // Adds endpoints for controller actions..
+            app.MapControllers();
 
             app.Run();
         }
