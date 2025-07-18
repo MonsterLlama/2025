17ac4dbeefe30c8073fbad8624a4552293430723
MonsterLlama <MonsterLlama@gmail.com>
2025-07-17 18:43:37 -0700

    Add GetReceiverById Action method to the Receivers controller.

--- Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Controllers/Receivers.cs
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Controllers/Receivers.cs
@@ -23,5 +23,23 @@ namespace Kiwi_SDR_Online_Receiver_Logging.Controllers
             // Test Return value
             return Ok(receiver);
         }
+
+        [HttpGet("id={id}")]
+        public IActionResult GetReceiverById(int id)
+        {
+            var receiver = new Receiver()
+            {
+                UrlId    = id,
+                URL      = "http://kb6c.proxy.kiwisdr.com:8073/",
+                Antenna  = "Wellbrooke Loop",
+                Name     = "KB6C/6",
+                Grid     = "DM04kr",
+                Location = "Stauffer, California",
+                ASL      = 1585
+            };
+
+            // Test Return value
+            return Ok(receiver);
+        }
     }
 }
