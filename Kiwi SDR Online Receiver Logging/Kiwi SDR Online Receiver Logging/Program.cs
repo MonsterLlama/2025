73933708e639923bf2f5c4619947a8fdd1f47907
MonsterLlama <MonsterLlama@gmail.com>
2025-07-22 21:26:08 -0700

    Add JwtBearerToken Authentication middleware support. Add appsettings.json and appsettings.Development.json to gitignore

--- Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Program.cs
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace MonsterLlama.KiwiSDR.Web.Logger
 {
     public class Program
@@ -11,10 +15,30 @@ namespace MonsterLlama.KiwiSDR.Web.Logger
             // Adds services for controllers to the services collection..
             builder.Services.AddControllers();
 
+            var secretKey = builder.Configuration.GetValue<string>("SecretKey");
+
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters 
+                    { 
+                        IssuerSigningKey         = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey ?? String.Empty)),
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime         = true,
+                        ValidateAudience         = false,
+                        ValidateIssuer           = false,
+                        ClockSkew                = TimeSpan.Zero
+                    };
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             // Adds endpoints for controller actions..
             app.MapControllers();
