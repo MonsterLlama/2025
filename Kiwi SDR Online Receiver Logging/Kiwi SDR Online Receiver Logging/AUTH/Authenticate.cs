07ccd33c0b8c4becf15e266b492f1e83a0ab7bd1
MonsterLlama <MonsterLlama@gmail.com>
2025-07-24 02:00:39 -0700

    Add extra properties to WebApiCredentials.  Implement Authentice Controller's AuthenticateClient Action Method.

--- Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/AUTH/Authenticate.cs
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/AUTH/Authenticate.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MonsterLlama.Kiwi_SDR_Online_Receiver_Logging.Auth.Model;
 using MonsterLlama.KiwiSDR.Web.Logger.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MonsterLlama.KiwiSDR.Web.Logger.AUTH
 {
@@ -11,10 +15,12 @@ namespace MonsterLlama.KiwiSDR.Web.Logger.AUTH
     public class Authenticate : ControllerBase
     {
         private readonly AuthenticationDbContext db;
+        private readonly IConfiguration configuration;
 
-        public Authenticate(AuthenticationDbContext authenticationDbContext)
+        public Authenticate(AuthenticationDbContext authenticationDbContext, IConfiguration configuration)
         {
             this.db = authenticationDbContext;
+            this.configuration = configuration;
         }
 
         [HttpGet]
@@ -25,6 +31,11 @@ namespace MonsterLlama.KiwiSDR.Web.Logger.AUTH
                 return BadRequest();
             }
 
+            if (creds is null || String.IsNullOrWhiteSpace(creds.ClientId) || String.IsNullOrWhiteSpace(creds.ClientSecret))
+            {
+                return BadRequest("You're unauthorized to access this resource.");
+            }
+
             // Validate Credentials
             var credsFound = db.Set<WebApiCredentials>().Any(c => c.ClientId == creds.ClientId && c.ClientSecret == creds.ClientSecret);
 
@@ -33,9 +44,44 @@ namespace MonsterLlama.KiwiSDR.Web.Logger.AUTH
                 return Unauthorized("You're unauthorized to access this resource.");
             }
 
-            // Create Jwt Security Token
+            // Create and populate the IEnumerable<Claim> object for the Jwt SecurityToken
+            var claims = new List<Claim>();
 
-           return Ok(creds);
+            claims.Add(new Claim("ClientName",       creds.ClientName));
+            claims.Add(new Claim("CanReadReceivers", creds.CanReadReceivers.ToString()));
+            claims.Add(new Claim("CanReadReceivers", creds.CanReadLogEntries.ToString()));
+            claims.Add(new Claim("CanReadReceivers", creds.CanAddReceiver.ToString()));
+            claims.Add(new Claim("CanReadReceivers", creds.CanAddLogEntry.ToString()));
+
+            // Create the SigningCredentials object for the Jwt SecurityToken
+            var secretKey          = this.configuration.GetValue<string>("SecretKey");
+            var securityKey        = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey ?? String.Empty));
+            var signingCredentials = new SigningCredentials(key: securityKey, algorithm: SecurityAlgorithms.HmacSha256Signature);
+
+            // Create Jwt Security Token
+            var jwt = new JwtSecurityToken(
+                claims:             claims,
+                notBefore:          DateTime.UtcNow,
+                expires:            DateTime.UtcNow.AddDays(1),
+                signingCredentials: signingCredentials
+                );
+
+            try
+            {
+                var token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+                return Ok(new {token, expires_at = jwt.ValidTo });
+            }
+            catch (SecurityTokenEncryptionFailedException e)
+            {
+                ModelState.AddModelError("JWT", e.Message);
+                return BadRequest(ModelState);
+            }
+            catch(Exception e)
+            {
+                ModelState.AddModelError("JWT", e.Message);
+                return BadRequest(ModelState);
+            }
         }
     }
 }
--- Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Auth/Model/WebApiCredentials.cs
+++ Kiwi SDR Online Receiver Logging/Kiwi SDR Online Receiver Logging/Auth/Model/WebApiCredentials.cs
@@ -12,9 +12,22 @@ namespace MonsterLlama.Kiwi_SDR_Online_Receiver_Logging.Auth.Model
         public string ClientId { get; set; } = string.Empty;
 
         [Required]
+        [Length(minimumLength: 36, maximumLength: 36, ErrorMessage = "Client Secret must have 36 characters.")]
         public string ClientSecret { get; set; }= string.Empty;
 
         [Required]
         public string ClientName {  get; set; } = string.Empty;
+
+        [Required]
+        public bool CanReadReceivers { get; set; } = false;
+
+        [Required]
+        public bool CanAddReceiver { get;set; } = false;
+
+        [Required]
+        public bool CanAddLogEntry { get; set; } = false;
+
+        [Required]
+        public bool CanReadLogEntries { get; set; } = false;
     }
 }
